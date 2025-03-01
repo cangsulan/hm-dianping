4ee29a8d3f4a020f8b8784ebe069d371d9a7f6af
cangsulan <3024179062@qq.com>
2025-03-01 13:45:36 +0800

    学到了好友关注-关注和取关

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,11 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -16,5 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+
+    @Resource
+    private IFollowService followService;
+
+    /**
+     * @description: 根据id，关注id所在的用户，或 取关 该用户
+     * @param: followUserId
+     * @param: isFollow
+     * @return: com.hmdp.dto.Result
+     * @author 30241
+     * @date: 2025/3/1 下午12:39
+     */
+    @PutMapping("/{id}/{isFollow}")
+    public Result follow(@PathVariable("id") Long followUserId, @PathVariable("isFollow") Boolean isFollow) {
+        return followService.follow(followUserId,isFollow);
+    }
+
+    /**
+     * @description: 查询当前用户是否关注了id所在的用户
+     * @param: followUserId
+     * @return: com.hmdp.dto.Result
+     * @author 30241
+     * @date: 2025/3/1 下午12:40
+     */
+    @GetMapping("/or/not/{id}")
+    public Result isFollow(@PathVariable("id") Long followUserId) {
+        return followService.isFollow(followUserId);
+    }
 
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,9 +1,12 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +20,34 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Override
+    public Result isFollow(Long followUserId) {
+        // 获取当前登录用户
+        Long userId = UserHolder.getUser().getId();
+        // 1.查询是否关注
+        Integer count = query().eq("follow_user_id", followUserId).eq("user_id", userId).count();
+
+        return Result.ok(count > 0);
+    }
+
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        // 获取当前登录用户
+        Long userId = UserHolder.getUser().getId();
+        // 1.判断是要 关注 还是 取关
+        if(isFollow){
+            // 2.关注，新增数据
+            Follow follow = new Follow();
+            follow.setFollowUserId(followUserId);
+            follow.setUserId(userId);
+            save(follow);
+        }else{
+            // 3.取关，删除数据
+            remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId)
+                    .eq("follow_user_id", followUserId));
+        }
+
+        return Result.ok();
+    }
 }
